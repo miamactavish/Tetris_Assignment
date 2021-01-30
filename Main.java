825570d9aa7fa733bb54290eb17aca12c02a8ed8
miamactavish <miamactavish@gmail.com>
2021-01-30 15:36:00 -0700

    Added equations

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int a = 4 + 3;
+        int b = a + 10;
     }
 
     public void testMethod() {

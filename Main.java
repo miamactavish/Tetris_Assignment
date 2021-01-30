44673b66e474edf4f7dcafbe6883e0d88c39fd9c
miamactavish <miamactavish@gmail.com>
2021-01-30 15:25:25 -0700

    Revert "Added new method in Main"
    This reverts commit bd7c4c33a072b537cdaf6cd833cb206c2b218174.

--- Main.java
+++ Main.java
@@ -2,8 +2,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-
-    public void testMethod() {
-        System.out.println("Hi");
-    }
 }

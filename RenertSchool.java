e31330f22a8fca76f53c1beb68c90a338c7bbd31
Kevin <77865332+kKEeWwon-2314@users.noreply.github.com>
2021-11-09 11:53:03 -0700

    Add files via upload

---
+++ RenertSchool.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class RenertSchool {
+    boolean serious;
+    boolean fun;
+
+    Scanner sc = new Scanner(System.in);
+
+    public boolean doOASISApp(int studentNumber, int temperature) {
+        return studentNumber;
+        return temperature;
+        
+        if (temperature < 28) {
+            return true;
+            System.out.println(studentNumber);
+        }
+        return false;
+    }
+    public void markTests(int score) {
+        return score;
+    }
+    public void ringMorningBell() {
+        serious = true;
+        fun = true;
+    }
+    public void setGooseTarget(String target) {
+        return target;
+    }
+
+    public void RenertSchool() {
+        System.out.println("lets go")
+
+        doOASISApp();
+        markTests();
+        ringMorningBell();
+        setGooseTarget("Sensei Tom");
+    }
+
+    public static void main(String args[]) {
+        RenertSchool();
+    }
+}
